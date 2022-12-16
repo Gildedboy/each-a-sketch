06c1343aa3b25ee3c6836b8d2331e0034716e0ef
Gilded <knukles30@gmail.com>
2022-12-15 22:01:42 -0700

    intial creation of grid and adding a hover status

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hola");
+const container = document.querySelector(".container");
+
+//Creation of completed Grid
+for (let i = 1; i <= 10; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
+  for (let i = 1; i <= 10; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+    for (let i = 1; i <= 2; i++) {
+      const insideRow = document.createElement("div");
+      insideRow.classList.add("insideRow");
+      square.appendChild(insideRow);
+      for (let i = 1; i <= 2; i++) {
+        const insideSquare = document.createElement("div");
+        insideSquare.classList.add("insideSquare");
+        insideRow.appendChild(insideSquare);
+      }
+    }
+  }
+}
+
+const pen = document.querySelector(".insideSquare");
