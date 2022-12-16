0982eebe36ca4d5a156d64906e52e2d3982d8c01
Gilded <knukles30@gmail.com>
2022-12-15 23:10:04 -0700

    adding some changes for being more beatiful

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const container = document.querySelector(".container");
-
+const grid = document.querySelector(".grid");
 //Creation of completed Grid
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 16; i++) {
   const row = document.createElement("div");
   row.classList.add("row");
   container.appendChild(row);
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 16; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     row.appendChild(square);
-    for (let i = 1; i <= 2; i++) {
-      const insideRow = document.createElement("div");
-      insideRow.classList.add("insideRow");
-      square.appendChild(insideRow);
-      for (let i = 1; i <= 2; i++) {
-        const insideSquare = document.createElement("div");
-        insideSquare.classList.add("insideSquare");
-        insideRow.appendChild(insideSquare);
-      }
-    }
   }
 }
 
-const pen = document.querySelector(".insideSquare");
+const pen = document.querySelectorAll(".square");
+pen.forEach((div) => {
+  div.addEventListener("click", function () {
+    div.classList.toggle("color");
+  });
+});
